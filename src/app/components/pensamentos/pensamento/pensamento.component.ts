5c08b030612da384f6bab0b75981ac9e6e826212
Wagner Costa <wagner.costa@bit.com.br>
2023-05-15 14:24:42 -0300

    :sparkles: feat: created new function atualizarFavorito

--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Pensamento } from '../pensamento.model';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
@@ -16,6 +17,8 @@ export class PensamentoComponent {
     favorito: false
   }
 
+  constructor(private service: PensamentoService) { }
+
   larguraPensamento(): string {
     if (this.pensamento.conteudo.length >= 256) {
       return 'pensamento-g'
@@ -28,5 +31,9 @@ export class PensamentoComponent {
       return 'inativo'
     }
     return 'ativo'
+  }
+
+  atualizarFavorito() {
+    this.service.mudarFavorito(this.pensamento).subscribe()
   }
 }
